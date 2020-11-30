77ac701ee2692804116d375de56f25f34205dbbb
Marco Auenhammer <marco.auenhammer@gmail.com>
2020-11-30 23:06:05 +0100

    extended CommentItem with some fields

--- mycrawler/items.py
+++ mycrawler/items.py
@@ -6,8 +6,7 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 from scrapy import Item, Field
-from dataclasses import dataclass
-from scrapy.loader.processors import MapCompose
+from itemloaders.processors import MapCompose, Compose, Join, TakeFirst
 import chompjs
 import re
 
@@ -24,6 +23,7 @@ class TestItem(Item):
 
 class StoryItem(Item):
     # TODO: add author shortnames from end of article
+    to_int = Compose(TakeFirst(), int)
 
     def parse_story_json_data(json_data):
         return chompjs.parse_js_object(json_data)
@@ -35,47 +35,58 @@ class StoryItem(Item):
         # TODO: check if country is correctly removed
         return re.sub('( aus \w*)', '', story_authors_data)
 
+    def sanitize_posting_count(string_data):
+        return re.sub('\W', '', string_data)
+
     # Story fields
-    story_url = Field()
+    story_url = Field(output_processor=Compose(lambda s: str(s[0])))
     story_authors = Field(input_processor=MapCompose(
         split_story_authors, sanitize_story_authors, str.strip))
     # TODO: fetch authors from story_content
+    # possible regex --> <p>.*\(([^)]+)\)<\/p>
     # story_authors_short = Field()
-    story_posting_count = Field()
+    story_posting_count = Field(
+        input_processor=Compose(lambda s: s[0], sanitize_posting_count), output_processor=Compose(lambda s: int(s[0])))
     story_breadcrumbs = Field()
-    story_publication_date = Field()
-    story_modification_date = Field()
+    story_publication_date = Field(input_processor=Compose(
+        lambda s: s[0], str.strip), output_processor=Compose(lambda s: str(s[0])))
+    story_modification_date = Field(input_processor=Compose(
+        lambda s: s[0], str.strip), output_processor=Compose(lambda s: str(s[0])))
     story_json_data = Field(input_processor=MapCompose(parse_story_json_data))
-    story_kicker = Field()
-    story_subtitle = Field()
-    story_title = Field()
+    story_kicker = Field(output_processor=Compose(lambda s: str(s[0])))
+    story_subtitle = Field(output_processor=Compose(lambda s: str(s[0])))
+    story_title = Field(output_processor=Compose(lambda s: str(s[0])))
     story_content = Field()
-    story_id = Field()
+    story_id = Field(output_processor=Compose(lambda s: int(s[0])))
+    # story_id = to_int
     # Crawler metadata
-    crawler_name = Field()
+    crawler_name = Field(output_processor=Compose(lambda s: str(s[0])))
     # First crawldate
-    store_date = Field()
+    store_date = Field(output_processor=Compose(lambda s: float(s[0])))
     # Last date of crawl
-    store_modification_date = Field()
+    store_modification_date = Field(
+        output_processor=Compose(lambda s: float(s[0])))
     # File
-    store_file = Field()
+    store_file = Field(output_processor=Join())
 
 
-class CommentItem():
+class CommentItem(Item):
     # TODO: sticky comment/from employee --> https://www.derstandard.at/story/2000122058320/wie-familien-trotz-corona-weihnachten-retten-koennen
-    comment_community_name = Field()
-    comment_url = Field()
-    comment_real_id = Field()
-    comment_organization_id = Field()
-    comment_supporter = Field()
-    comment_verified_id = Field()
-    comment_posting_id = Field()
-    comment_parent_posting_id = Field()
-    comment_community_id = Field()
-    comment_community_profile = Field()
-    comment_postdate = Field()
+    comment_story_id = Field()
+    # comment_community_name = Field()
+    # comment_url = Field()
+    # comment_real_id = Field()
+    # comment_organization_id = Field()
+    # comment_supporter = Field()
+    # comment_verified_id = Field()
+    # comment_posting_id = Field()
+    # comment_parent_posting_id = Field()
+    # comment_community_id = Field()
+    # comment_community_profile = Field()
+    # comment_postdate = Field()
+    # comment_title = Field()
     comment_content = Field()
-    comment_ratings_positive = Field()
-    comment_ratings_negative = Field()
-    comment_follower = Field()
-    store_date = Field()
+    # comment_ratings_positive = Field()
+    # comment_ratings_negative = Field()
+    # comment_follower = Field()
+    # store_date = Field()
