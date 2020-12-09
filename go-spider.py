7e5aac79adb1c22f57efd6225c9982266dbb34d4
Marco Auenhammer <marco.auenhammer@gmail.com>
2020-12-09 15:46:28 +0100

    removal of old files adjusted docker-compose file

--- go-spider.py
+++
@@ -1,9 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
-from mycrawler.spiders.pageavailability import PageavailabilitySpider
-from mycrawler.spiders.derstandard import DerStandardCrawler
-
-process = CrawlerProcess(get_project_settings())
-process.crawl(DerStandardCrawler)
-process.start()
